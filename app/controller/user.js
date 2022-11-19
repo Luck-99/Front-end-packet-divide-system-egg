42885d0784e01767b7c6736fcc80bebd1e2e263b
Luck-99 <1427703250@qq.com>
2022-11-19 20:06:08 +0800

    feat：user请求测试学习

--- app/controller/user.js
+++ app/controller/user.js
@@ -7,14 +7,34 @@ class UserController extends Controller {
     const { ctx } = this;
     ctx.body = '<h1>user</h1>';
   }
-
-  async getUsers() {
+  async getUsers() { // 异步模式
     const { ctx } = this;
     await new Promise(resolve => {
       setTimeout(() => {
-        resolve(ctx.body = '<h1>一些用户</h1>');
+        resolve((ctx.body = '<h1>一些用户</h1>'));
       }, 1000);
     });
   }
+  async getUser() {
+    // 自由传参模式
+    const { ctx } = this;
+    const { name } = ctx.query;
+    ctx.body = '姓名' + name + '所有参数：' + JSON.stringify(ctx.query);
+  }
+  async getUser2() {
+    // 严格传参模式
+    const { ctx } = this;
+    const { name, age } = ctx.params;
+    ctx.body = '姓名:' + name + '年龄:' + age;
+  }
+
+  async addUser() { // post方法
+    const { ctx } = this;
+    ctx.body = {
+      status: 200,
+      data: ctx.request.body,
+    };
+  }
+
 }
 module.exports = UserController;
--- app/router.js
+++ app/router.js
@@ -10,4 +10,7 @@ module.exports = app => {
   router.get('/xz', controller.home.xz);
   router.get('/user', controller.user.index);
   router.get('/getUsers', controller.user.getUsers);
+  router.get('/getUser', controller.user.getUser);
+  router.get('/getUser2/:name/:age', controller.user.getUser2);
+  router.post('/addUser', controller.user.addUser);
 };
