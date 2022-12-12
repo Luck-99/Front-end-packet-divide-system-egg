5e59bba8cebc08748eea1427f851313157e7873d
zhangzelin <zhangzelin@apexsoft.com.cn>
2022-12-12 16:45:53 +0800

    style：优化代码，无改动

--- app/schedule/getAllProjects.js
+++ app/schedule/getAllProjects.js
@@ -1,17 +1,23 @@
 'use strict'
 const Subscription = require('egg').Subscription
 
+/**
+ * 用于获取所有项目信息
+ */
 module.exports = {
   schedule: {
     interval: '5s', // 1 分钟间隔
     type: 'all', // 指定所有的 worker 都需要执行
   },
   async task(ctx) {
-    const { app, service } = ctx
+    const {
+      app,
+      service: { file },
+    } = ctx
     const {
       config: { PROJECTENVSNAME },
     } = app
-    const data = await service.file.readFile(PROJECTENVSNAME)
+    const data = await file.readFile(PROJECTENVSNAME)
     const nsp = app.io.of('/')
     nsp.emit('jenkinsAllJobs', data)
   },
