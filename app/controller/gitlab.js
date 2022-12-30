2b6a47419a61df99f9b73eac44b72459156de07b
zhangzelin <zhangzelin@apexsoft.com.cn>
2022-12-30 15:50:37 +0800

    refactor:删除冗余trycatch

--- app/controller/gitlab.js
+++ app/controller/gitlab.js
@@ -5,28 +5,26 @@ const Controller = require('./base_controller')
 class GitlabController extends Controller {
   async getProjects() {
     // 需要用来获取name和项目id
-    try {
-      const {
-        ctx,
-        config: { GITLABAPI, GITLABTOKEN },
-      } = this
-      const res = await ctx.curl(`${GITLABAPI}/projects`, {
-        method: 'GET',
-        data: {
-          private_token: GITLABTOKEN,
-          visibility: 'private', //私有项目
-          per_page: 10000,
-        },
-        dataType: 'json',
-      })
-      if (res.status === 200) {
-        this.success('获取项目成功', res.data)
-      } else {
-        this.failed('获取失败', res)
-      }
-    } catch (error) {
-      this.failed(error.message)
+    const {
+      ctx,
+      config: { GITLABAPI, GITLABTOKEN },
+    } = this
+    const res = await ctx.curl(`${GITLABAPI}/projects`, {
+      method: 'GET',
+      data: {
+        private_token: GITLABTOKEN,
+        visibility: 'private', //私有项目
+        per_page: 10000,
+      },
+      dataType: 'json',
+    })
+    if (res.status === 200) {
+      this.success('获取项目成功', res.data)
+      return res.data
+    } else {
+      this.failed('获取失败', res)
     }
+    return []
   }
 
   async getProjectCommits() {
